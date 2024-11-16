c1fec26868d843789d568ffe85a539e9a164a476
Chukwuemeka Nelson Nwauche <cnwauche@gmail.com>
2024-11-16 17:39:10 -0500

    Added initial files for ffplay to output an Imgui textbox in an SDL window

---
+++ ffplay/build-and-run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Check if build directory exists and remove it if it does
+if [ -d "build" ]; then
+    echo "Removing existing build directory..."
+    rm -rf build
+fi
+
+# Create and enter build directory
+mkdir build
+cd build
+
+# Run CMake
+cmake ..
+
+# Build the project
+cmake --build .
+
+# Check if build was successful
+if [ $? -eq 0 ]; then
+    echo "Build successful. Running ffplay..."
+    # Run the executable
+    ./ffplay
+else
+    echo "Build failed. Please check the error messages above."
+fi
--- ffplay/ffplay.cpp
+++ ffplay/ffplay.cpp
@@ -1,8 +1,8 @@
-
 #include "imgui.h"
 #include "imgui_impl_sdl2.h"
 #include "imgui_impl_sdlrenderer2.h"
 #include <SDL2/SDL.h>
+#include <stdio.h>  // for printf
 
 #if !SDL_VERSION_ATLEAST(2,0,17)
 #error This backend requires SDL 2.0.17+ because of SDL_RenderGeometry() function
@@ -10,33 +10,51 @@
 
 int main(int, char**)
 {
+    // Initialize SDL
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0)
     {
         printf("Error: %s\n", SDL_GetError());
         return -1;
     }
 
+    // Create window
     SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
     SDL_Window* window = SDL_CreateWindow("Dear ImGui SDL2+SDL_Renderer example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
-    if (renderer == nullptr)
+    if (window == NULL)
     {
-        SDL_Log("Error creating SDL_Renderer!");
-        return 0;
+        printf("Error creating window: %s\n", SDL_GetError());
+        SDL_Quit();
+        return -1;
     }
 
+    // Create renderer
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL)
+    {
+        SDL_Log("Error creating SDL_Renderer: %s", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return -1;
+    }
+
+    // Setup Dear ImGui context
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
 
+    // Setup Dear ImGui style
     ImGui::StyleColorsDark();
 
+    // Setup Platform/Renderer backends
     ImGui_ImplSDL2_InitForSDLRenderer(window, renderer);
     ImGui_ImplSDLRenderer2_Init(renderer);
 
+    // Main loop
     bool done = false;
     while (!done)
     {
+        // Poll and handle events
         SDL_Event event;
         while (SDL_PollEvent(&event))
         {
@@ -47,22 +65,26 @@ int main(int, char**)
                 done = true;
         }
 
+        // Start the Dear ImGui frame
         ImGui_ImplSDLRenderer2_NewFrame();
         ImGui_ImplSDL2_NewFrame();
         ImGui::NewFrame();
 
+        // Your ImGui windows and controls go here
         ImGui::Begin("Hello, world!");
         ImGui::Text("This is some useful text.");
         ImGui::End();
 
+        // Rendering
         ImGui::Render();
         SDL_RenderSetScale(renderer, io.DisplayFramebufferScale.x, io.DisplayFramebufferScale.y);
         SDL_SetRenderDrawColor(renderer, (Uint8)(0.45f * 255), (Uint8)(0.55f * 255), (Uint8)(0.60f * 255), (Uint8)(255));
         SDL_RenderClear(renderer);
-        ImGui_ImplSDLRenderer2_RenderDrawData(ImGui::GetDrawData());
+        ImGui_ImplSDLRenderer2_RenderDrawData(ImGui::GetDrawData(), renderer);
         SDL_RenderPresent(renderer);
     }
 
+    // Cleanup
     ImGui_ImplSDLRenderer2_Shutdown();
     ImGui_ImplSDL2_Shutdown();
     ImGui::DestroyContext();
@@ -73,6 +95,13 @@ int main(int, char**)
 
     return 0;
 }
+
+// extern "C" {
+// #include <libavcodec/avcodec.h>
+// #include <libavformat/avformat.h>
+// // other FFmpeg headers
+// }
+
 
 // /*
 //  * Copyright (c) 2003 Fabrice Bellard
