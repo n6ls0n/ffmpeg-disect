134ce59ba026c907c313f9fc755b32562388d5c2
Chukwuemeka Nelson Nwauche <cnwauche@gmail.com>
2024-11-17 16:50:33 -0500

    Added to Audio Functions

--- ffplay/ffplay.cpp
+++ ffplay/ffplay.cpp
@@ -2460,6 +2460,17 @@ static void sdl_audio_callback(void *opaque, Uint8 *stream, int len)
     }
 }
 
+static inline
+int cmp_audio_fmts(enum AVSampleFormat fmt1, int64_t channel_count1,
+                   enum AVSampleFormat fmt2, int64_t channel_count2)
+{
+    /* If channel count == 1, planar and non-planar formats are the same */
+    if (channel_count1 == 1 && channel_count2 == 1)
+        return av_get_packed_sample_fmt(fmt1) != av_get_packed_sample_fmt(fmt2);
+    else
+        return channel_count1 != channel_count2 || fmt1 != fmt2;
+}
+
 
 //              ##########################################
 //                         Video Functions
@@ -3220,16 +3231,6 @@ end:
 
 
 
-static inline
-int cmp_audio_fmts(enum AVSampleFormat fmt1, int64_t channel_count1,
-                   enum AVSampleFormat fmt2, int64_t channel_count2)
-{
-    /* If channel count == 1, planar and non-planar formats are the same */
-    if (channel_count1 == 1 && channel_count2 == 1)
-        return av_get_packed_sample_fmt(fmt1) != av_get_packed_sample_fmt(fmt2);
-    else
-        return channel_count1 != channel_count2 || fmt1 != fmt2;
-}
 
 static inline void fill_rectangle(int x, int y, int w, int h)
 {
