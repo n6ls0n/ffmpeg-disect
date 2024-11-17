a39cb53a5ff0d0159ef075fd8f3d017976eefbf7
Chukwuemeka Nelson Nwauche <cnwauche@gmail.com>
2024-11-17 14:52:46 -0500

    Grouped Frame functions

--- ffplay/ffplay.cpp
+++ ffplay/ffplay.cpp
@@ -832,16 +832,10 @@ static int decoder_start(Decoder *d, int (*fn)(void *), const char *thread_name,
     return 0;
 }
 
-static inline
-int cmp_audio_fmts(enum AVSampleFormat fmt1, int64_t channel_count1,
-                   enum AVSampleFormat fmt2, int64_t channel_count2)
-{
-    /* If channel count == 1, planar and non-planar formats are the same */
-    if (channel_count1 == 1 && channel_count2 == 1)
-        return av_get_packed_sample_fmt(fmt1) != av_get_packed_sample_fmt(fmt2);
-    else
-        return channel_count1 != channel_count2 || fmt1 != fmt2;
-}
+
+//              ##########################################
+//                         Frame Functions
+//              ##########################################
 
 static void frame_queue_unref_item(Frame *vp)
 {
@@ -978,6 +972,16 @@ static int64_t frame_queue_last_pos(FrameQueue *f)
 }
 
 
+static inline
+int cmp_audio_fmts(enum AVSampleFormat fmt1, int64_t channel_count1,
+                   enum AVSampleFormat fmt2, int64_t channel_count2)
+{
+    /* If channel count == 1, planar and non-planar formats are the same */
+    if (channel_count1 == 1 && channel_count2 == 1)
+        return av_get_packed_sample_fmt(fmt1) != av_get_packed_sample_fmt(fmt2);
+    else
+        return channel_count1 != channel_count2 || fmt1 != fmt2;
+}
 
 static inline void fill_rectangle(int x, int y, int w, int h)
 {
