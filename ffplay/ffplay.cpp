c86bbe4759f372a9bdbaa916aca42507fe5253bc
Chukwuemeka Nelson Nwauche <cnwauche@gmail.com>
2024-11-24 16:25:04 -0500

    Added cross-platform support for both GUI and console mode

--- ffplay/ffplay.cpp
+++ ffplay/ffplay.cpp
@@ -7,9 +7,12 @@
 #include <signal.h>
 #include <stdint.h>
 
+#ifdef BUILD_AS_GUI
 #include "imgui.h"
 #include "imgui_impl_sdl2.h"
 #include "imgui_impl_sdlrenderer2.h"
+#endif
+
 #include <SDL2/SDL.h>
 #include <stdio.h>
 
@@ -3917,98 +3920,140 @@ static void event_loop(VideoState *cur_stream)
 // =============================================================================
 
 #ifdef _WIN32
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
-#else
-int main(int argc, char** argv)
+#include <SDL2/SDL_main.h>
 #endif
-// int main(int argc, char** argv)
+
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+#ifdef BUILD_AS_GUI
+#include "imgui.h"
+#include "imgui_impl_sdl2.h"
+#include "imgui_impl_sdlrenderer2.h"
+#endif
+
+extern "C" int SDL_main(int argc, char** argv)
 {
     int flags, ret;
     VideoState *is;
 
     init_dynload();
 
+    av_log_set_flags(AV_LOG_SKIP_REPEATED);
+    parse_loglevel(argc, argv, options);
+
     // Initialize SDL
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0)
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER) != 0)
     {
-        printf("Error: %s\n", SDL_GetError());
+        fprintf(stderr, "Error: %s\n", SDL_GetError());
         return -1;
     }
 
-    // Create window
-    SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
-    SDL_Window* window = SDL_CreateWindow("Dear ImGui SDL2+SDL_Renderer example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
-    if (window == NULL)
-    {
-        printf("Error creating window: %s\n", SDL_GetError());
-        SDL_Quit();
-        return -1;
-    }
-
-    // Create renderer
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
-    if (renderer == NULL)
-    {
-        SDL_Log("Error creating SDL_Renderer: %s", SDL_GetError());
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return -1;
-    }
-
-    // Setup Dear ImGui context
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
-
-    // Setup Dear ImGui style
-    ImGui::StyleColorsDark();
-
-    // Setup Platform/Renderer backends
-    ImGui_ImplSDL2_InitForSDLRenderer(window, renderer);
-    ImGui_ImplSDLRenderer2_Init(renderer);
-
-    // Main loop
-    bool done = false;
-    while (!done)
-    {
-        // Poll and handle events
-        SDL_Event event;
-        while (SDL_PollEvent(&event))
+    #ifdef BUILD_AS_GUI
+        // Create window
+        SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
+        SDL_Window* window = SDL_CreateWindow("FFplay with ImGui", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
+        if (window == NULL)
         {
-            ImGui_ImplSDL2_ProcessEvent(&event);
-            if (event.type == SDL_QUIT)
-                done = true;
-            if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE && event.window.windowID == SDL_GetWindowID(window))
-                done = true;
+            fprintf(stderr, "Error creating window: %s\n", SDL_GetError());
+            SDL_Quit();
+            return -1;
         }
 
-        // Start the Dear ImGui frame
-        ImGui_ImplSDLRenderer2_NewFrame();
-        ImGui_ImplSDL2_NewFrame();
-        ImGui::NewFrame();
+        // Create renderer
+        SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
+        if (renderer == NULL)
+        {
+            SDL_Log("Error creating SDL_Renderer: %s", SDL_GetError());
+            SDL_DestroyWindow(window);
+            SDL_Quit();
+            return -1;
+        }
 
-        // Your ImGui windows and controls go here
-        // ImGui::Begin("Hello, world!");
-        // ImGui::Text("This is some useful text.");
-        // ImGui::End();
+        // Setup Dear ImGui context
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImGuiIO& io = ImGui::GetIO(); (void)io;
+        io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
 
-        // Rendering
-        ImGui::Render();
-        SDL_RenderSetScale(renderer, io.DisplayFramebufferScale.x, io.DisplayFramebufferScale.y);
-        SDL_SetRenderDrawColor(renderer, (Uint8)(0.45f * 255), (Uint8)(0.55f * 255), (Uint8)(0.60f * 255), (Uint8)(255));
-        SDL_RenderClear(renderer);
-        ImGui_ImplSDLRenderer2_RenderDrawData(ImGui::GetDrawData(), renderer);
-        SDL_RenderPresent(renderer);
-    }
+        // Setup Dear ImGui style
+        ImGui::StyleColorsDark();
 
-    // Cleanup
-    ImGui_ImplSDLRenderer2_Shutdown();
-    ImGui_ImplSDL2_Shutdown();
-    ImGui::DestroyContext();
+        // Setup Platform/Renderer backends
+        ImGui_ImplSDL2_InitForSDLRenderer(window, renderer);
+        ImGui_ImplSDLRenderer2_Init(renderer);
 
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
+        // Main loop
+        bool done = false;
+        while (!done)
+        {
+            // Poll and handle events
+            SDL_Event event;
+            while (SDL_PollEvent(&event))
+            {
+                ImGui_ImplSDL2_ProcessEvent(&event);
+                if (event.type == SDL_QUIT)
+                    done = true;
+                if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE && event.window.windowID == SDL_GetWindowID(window))
+                    done = true;
+            }
+
+            // Start the Dear ImGui frame
+            ImGui_ImplSDLRenderer2_NewFrame();
+            ImGui_ImplSDL2_NewFrame();
+            ImGui::NewFrame();
+
+            // Your ImGui windows and controls go here
+            ImGui::Begin("FFplay Controls");
+            ImGui::Text("Add your controls here");
+            ImGui::End();
+
+            // Rendering
+            ImGui::Render();
+            SDL_RenderSetScale(renderer, io.DisplayFramebufferScale.x, io.DisplayFramebufferScale.y);
+            SDL_SetRenderDrawColor(renderer, (Uint8)(0.45f * 255), (Uint8)(0.55f * 255), (Uint8)(0.60f * 255), (Uint8)(255));
+            SDL_RenderClear(renderer);
+            ImGui_ImplSDLRenderer2_RenderDrawData(ImGui::GetDrawData(), renderer);
+            SDL_RenderPresent(renderer);
+        }
+
+        // Cleanup
+        ImGui_ImplSDLRenderer2_Shutdown();
+        ImGui_ImplSDL2_Shutdown();
+        ImGui::DestroyContext();
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+    #else
+
+        // Create a window
+        SDL_Window* window = SDL_CreateWindow("FFplay Console Mode", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+        if (window == NULL)
+        {
+            fprintf(stderr, "Error creating window: %s\n", SDL_GetError());
+            SDL_Quit();
+            return -1;
+        }
+
+        // Show the window
+        SDL_ShowWindow(window);
+
+        // Wait for the window to be closed
+        bool done = false;
+        while (!done)
+        {
+            SDL_Event event;
+            while (SDL_PollEvent(&event))
+            {
+                if (event.type == SDL_QUIT)
+                    done = true;
+            }
+            SDL_Delay(100);
+        }
+
+        // Cleanup
+        SDL_DestroyWindow(window);
+    #endif
+
     SDL_Quit();
 
     return 0;
